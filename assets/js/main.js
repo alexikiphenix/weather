8049ef84b127726fdb0eee74d6a846e7ec526491
alexikiphenix <alexikiphenix@gmail.com>
2023-08-20 23:59:30 +0200

    adding temperature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+const temperature = document.getElementById(`temperature`);
 const temperatureLabel = document.getElementById(`temperature_label`);
 const city = 'Tokyo';
 const key = '3ba750e375ce856d1fecdb1aa520f18e';
