d636d19df242c39b0f8485713e2a27e0ae6e4aac
alexikiphenix <alexikiphenix@gmail.com>
2023-08-21 17:58:48 +0200

    adding fn checkInput v1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,18 +10,13 @@ const key = '3ba750e375ce856d1fecdb1aa520f18e';
 const urlOFF = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`;
 
 
-const temperatureBox = document.createElement('input');
-temperatureBox.setAttribute('id', 'inputCity');
+const inputCity = document.createElement('input');
+inputCity.setAttribute('id', 'inputCity');
 
-btnChange.addEventListener('click', () =>
-{
-    if(temperatureBox.innerText);
-    box.prepend(temperatureBox);
-})
 
 const getTemperature = (city) =>
 {
-    if(city === string);
+    if(typeof city === 'string' || city instanceof String);
     else
         city = 'Paris';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
@@ -41,7 +36,43 @@ const getTemperature = (city) =>
                 temperatureLabel.innerText = `${myData.main.temp}`;
             }
             else
-                console.log('Erreur informations non reçues');
+            console.log('Erreur informations non reçues');
         }
     }
 }
+
+const checkInput = (inputUser = String) =>
+{
+    if(typeof inputUser.value === 'string' 
+        || inputUser.value instanceof String              
+    )
+    {
+        if(inputUser.value.length >= 2)
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+getTemperature('San francisco');
+
+
+// inputCity.value = "GROS TEST";
+btnChange.addEventListener('click', () =>
+{      
+    if(checkInput(inputCity))
+    {
+        console.log(inputCity.value.length);
+        getTemperature(inputCity.value);
+    }
+
+    if(!document.getElementById('inputCity')) 
+    {
+        box.prepend(inputCity);   
+    }
+    box.prepend(inputCity);
+})
+
+
