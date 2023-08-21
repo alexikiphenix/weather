603db18dbc81c4ec5dc0a99c2fbee3e563f686bf
alexikiphenix <alexikiphenix@gmail.com>
2023-08-21 15:22:45 +0200

    adding box for seizing city name

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@
 const temperature = document.getElementById(`temperature`);
 const temperatureLabel = document.getElementById(`temperature_label`);
 const btnChange = document.querySelector(`#change`);
+const box = document.querySelector('#box');
 const cityBox = document.querySelector('#city');
 let city = 'Tokyo';
 const key = '3ba750e375ce856d1fecdb1aa520f18e';
 
 const urlOFF = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
 
 const temperatureBox = document.createElement('input');
@@ -15,27 +15,33 @@ temperatureBox.setAttribute('id', 'inputCity');
 
 btnChange.addEventListener('click', () =>
 {
-    temperature.prepend(temperatureBox);
+    if(temperatureBox.innerText);
+    box.prepend(temperatureBox);
 })
-const myRequest = new XMLHttpRequest();
-myRequest.open('GET', url);
-myRequest.responseType = 'json';
-myRequest.send();
 
-
-myRequest.onload = function()
+const getTemperature = (city) =>
 {
-    if(myRequest.readyState === XMLHttpRequest.DONE)
+    if(city === string);
+    else
+        city = 'Paris';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    const myRequest = new XMLHttpRequest();
+    myRequest.open('GET', url);
+    myRequest.responseType = 'json';
+    myRequest.send();
+    myRequest.onload = function()
     {
-        if(myRequest.status === 200)
+        if(myRequest.readyState === XMLHttpRequest.DONE)
         {
-            const myData = myRequest.response;
-            console.log(myData);
-            cityBox.innerText = `${myData.name}`;
-            temperatureLabel.innerText = `${myData.main.temp}`;
+            if(myRequest.status === 200)
+            {
+                const myData = myRequest.response;
+                console.log(myData);
+                cityBox.innerText = `${myData.name}`;
+                temperatureLabel.innerText = `${myData.main.temp}`;
+            }
+            else
+                console.log('Erreur informations non reçues');
         }
-        else
-            console.log('Erreur informations non reçues');
     }
 }
-
